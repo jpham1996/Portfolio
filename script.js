a711afdf0a77503147ad81ecddcbb0dbb456dfc1
Jamie Pham <jamiepham14@gmail.com>
2023-12-30 09:37:03 -0500

    Add hero section, about section, projects section, and contact section

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
+const hamburger = document.querySelector(".header__hamburger");
+const navMenu = document.querySelector(".header__main");
+const navLinks = document.querySelectorAll(".header-link");
 
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle("nav--visible");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
+
+navLinks.forEach((n) =>
+  n.addEventListener("click", function () {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
