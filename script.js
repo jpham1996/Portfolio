357dcf6c85ba4c471fff67bc6939d90ae9b017c0
Jamie Pham <jamiepham14@gmail.com>
2024-01-01 14:28:28 -0500

    Add footer, changed images from png to svg, rework on project section, add more skills in the about section

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector(".header__hamburger");
 const navMenu = document.querySelector(".header__main");
 const navLinks = document.querySelectorAll(".header-link");
+const currentYear = document.querySelector("#currentYear");
+
+const year = new Date().getFullYear();
+currentYear.innerText = year;
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
