26426fc17fe1ca6eba6007e7610099dda41eadf2
Jamie Pham <jamiepham14@gmail.com>
2024-01-04 18:37:17 -0500

    Fix minor JS bug, made project images clear and not squished, fix minor mobile responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-import WEB3FORMS_API_KEY from "./apikey.js";
-
 const hamburger = document.querySelector(".header__hamburger");
 const navMenu = document.querySelector(".header__main");
 const navLinks = document.querySelectorAll(".header-link");
 const currentYear = document.querySelector("#currentYear");
-const apiKey = document.querySelector("#apiKey");
-
-apiKey.value = WEB3FORMS_API_KEY;
 
 const year = new Date().getFullYear();
 currentYear.innerText = year;
