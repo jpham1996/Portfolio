48ea577d17fa9f060b645fca8fa777fb49c4fe35
Jamie Pham <jamiepham14@gmail.com>
2024-01-04 17:21:14 -0500

    Fixed font size with contact inputs, removed social h2 title from footer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import Web3Forms_API_KEY from "./apikey.js";
+import WEB3FORMS_API_KEY from "./apikey.js";
 
 const hamburger = document.querySelector(".header__hamburger");
 const navMenu = document.querySelector(".header__main");
@@ -6,7 +6,7 @@ const navLinks = document.querySelectorAll(".header-link");
 const currentYear = document.querySelector("#currentYear");
 const apiKey = document.querySelector("#apiKey");
 
-apiKey.value = Web3Forms_API_KEY;
+apiKey.value = WEB3FORMS_API_KEY;
 
 const year = new Date().getFullYear();
 currentYear.innerText = year;
