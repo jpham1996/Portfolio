3d38063372ae548fc710cad468e18e18942b9448
Jamie Pham <jamiepham14@gmail.com>
2024-01-04 10:56:08 -0500

    Hide API Key, fix font sizes with ems and rems, and removed placebo hero image and added real image

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+import Web3Forms_API_KEY from "./apikey.js";
+
 const hamburger = document.querySelector(".header__hamburger");
 const navMenu = document.querySelector(".header__main");
 const navLinks = document.querySelectorAll(".header-link");
 const currentYear = document.querySelector("#currentYear");
+const apiKey = document.querySelector("#apiKey");
+
+apiKey.value = Web3Forms_API_KEY;
 
 const year = new Date().getFullYear();
 currentYear.innerText = year;
